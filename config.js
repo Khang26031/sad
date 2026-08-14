5a382377b746c5fa7b6560090d256414366d5c28
Khang26031 <khang2603@gmail.com>
2026-08-14 14:37:16 +0700

    Update Supabase Anon Key with correct JWT token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.SUPABASE_CONFIG = {
   url: 'https://agewogarchceqcoxdxgi.supabase.co',
-  anonKey: 'sb_publishable_8nUBX29oNc0f674qRVxeVA_AShdfnrR'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnZXdvZ2FyY2hjZXFjb3hkeGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2ODc3NTcsImV4cCI6MjEwMjI2Mzc1N30.vtjcLZIxkFx43DEYyFSoPMtJcoQ5GNoSyMVcJIv4LQU'
 };
